3dfda2732da8475376632a78ad4cf4911c2db14c
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-05 07:33:07 +0000

    feat: integrate serverless-http and configure Vercel deployment

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+// index.js
+
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import serverless from "serverless-http";
 
-
+import postsRouter from "./routes/post.routes.js";
+import todosRouter from "./routes/todo.routes.js";
+import usersRouter from "./routes/user.routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-global.__rootdir = __dirname; // used inside route files
+global.__rootdir = __dirname;
 
 const app = express();
 
-// posts 
-import postsRouter from "./routes/post.routes.js";
+// routers
 app.use("/api/posts", postsRouter);
-// todos 
-import todosRouter from "./routes/todo.routes.js";
 app.use("/api/todos", todosRouter);
-// users
-import usersRouter from "./routes/user.routes.js";
 app.use("/api/users", usersRouter);
 
+// serverless export (for vercel)
+export const handler = serverless(app);
 
-// server running 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server Running on http://localhost:${PORT}`));
+// auto local mode
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`LOCAL: http://localhost:${PORT}`));
+}
